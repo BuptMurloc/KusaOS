4805a0a4ea3f79f55efbb39d8e267aa13cc4c375
Big_K <54524841+BuptMurloc@users.noreply.github.com>
2021-04-18 12:08:41 +0800

    注释
    每个函数的用途都写了注释，部分函数的内容写了些注释

--- fs/misc.c
+++ fs/misc.c
@@ -25,26 +25,26 @@
 /*****************************************************************************
  *                                do_stat
  *************************************************************************//**
- * Perform the stat() syscall.
+ * 作用是得到文件的信息，在这里我们主要关心其大小
  * 
  * @return  On success, zero is returned. On error, -1 is returned.
  *****************************************************************************/
 PUBLIC int do_stat()
 {
-	char pathname[MAX_PATH]; /* parameter from the caller */
+	char pathname[MAX_PATH]; /* 来自调用者的参数 */
 	char filename[MAX_PATH]; /* directory has been stipped */
 
 	/* get parameters from the message */
-	int name_len = fs_msg.NAME_LEN;	/* length of filename */
-	int src = fs_msg.source;	/* caller proc nr. */
+	int name_len = fs_msg.NAME_LEN;	/* 文件名长度 */
+	int src = fs_msg.source;	/* 调用者的进程号. */
 	assert(name_len < MAX_PATH);
-	phys_copy((void*)va2la(TASK_FS, pathname),    /* to   */
-		  (void*)va2la(src, fs_msg.PATHNAME), /* from */
+	phys_copy((void*)va2la(TASK_FS, pathname),    /* 目的地   */
+		  (void*)va2la(src, fs_msg.PATHNAME), /* 来源 */
 		  name_len);
 	pathname[name_len] = 0;	/* terminate the string */
 
 	int inode_nr = search_file(pathname);
-	if (inode_nr == INVALID_INODE) {	/* file not found */
+	if (inode_nr == INVALID_INODE) {	/* 文件没找到 */
 		printl("{FS} FS::do_stat():: search_file() returns "
 		       "invalid inode: %s\n", pathname);
 		return -1;
@@ -62,7 +62,7 @@ PUBLIC int do_stat()
 	}
 	pin = get_inode(dir_inode->i_dev, inode_nr);
 
-	struct stat s;		/* the thing requested */
+	struct stat s;		/* 请求的东西 */
 	s.st_dev = pin->i_dev;
 	s.st_ino = pin->i_num;
 	s.st_mode= pin->i_mode;
@@ -71,8 +71,8 @@ PUBLIC int do_stat()
 
 	put_inode(pin);
 
-	phys_copy((void*)va2la(src, fs_msg.BUF), /* to   */
-		  (void*)va2la(TASK_FS, &s),	 /* from */
+	phys_copy((void*)va2la(src, fs_msg.BUF), /* 目的地   */
+		  (void*)va2la(TASK_FS, &s),	 /* 来源 */
 		  sizeof(struct stat));
 
 	return 0;
@@ -82,7 +82,9 @@ PUBLIC int do_stat()
  *                                search_file
  *****************************************************************************/
 /**
- * Search the file and return the inode_nr.
+ * 我们还是通过strip_path( )来得到文件所在目录的i-node，通过这个i-node来得到目录
+ * 所在的扇区，然后读取这些扇区，查看里面是否有我们要找的文件，如果找到就返回文件
+ * 的i-node，如果没找到就返回零。
  *
  * @param[in] path The full path of the file to search.
  * @return         Ptr to the i-node of the file if successful, otherwise zero.
@@ -100,11 +102,11 @@ PUBLIC int search_file(char * path)
 	if (strip_path(filename, path, &dir_inode) != 0)
 		return 0;
 
-	if (filename[0] == 0)	/* path: "/" */
+	if (filename[0] == 0)	/* 路径 "/" */
 		return dir_inode->i_num;
 
 	/**
-	 * Search the dir for the file.
+	 * 找到文件的目录
 	 */
 	int dir_blk0_nr = dir_inode->i_start_sect;
 	int nr_dir_blks = (dir_inode->i_size + SECTOR_SIZE - 1) / SECTOR_SIZE;
@@ -125,11 +127,11 @@ PUBLIC int search_file(char * path)
 			if (++m > nr_dir_entries)
 				break;
 		}
-		if (m > nr_dir_entries) /* all entries have been iterated */
+		if (m > nr_dir_entries) /* 所有目录项都已经被遍历 */
 			break;
 	}
 
-	/* file not found */
+	/* 文件没找到 */
 	return 0;
 }
 
@@ -137,13 +139,13 @@ PUBLIC int search_file(char * path)
  *                                strip_path
  *****************************************************************************/
 /**
- * Get the basename from the fullpath.
+ * 把路径分成文件名和文件夹两部分
  *
- * In Orange'S FS v1.0, all files are stored in the root directory.
- * There is no sub-folder thing.
+ * 在当前文件系统中，只支持根目录
+ * 没有子目录
  *
- * This routine should be called at the very beginning of file operations
- * such as open(), read() and write(). It accepts the full path and returns
+ * 将在文件操作的开头被调用，如open(), read() and write(). 
+ * 接收一个完整的路径并返回两个东西：文件名和目录inode指针
  * two things: the basename and a ptr of the root dir's i-node.
  *
  * e.g. After stip_path(filename, "/blah", ppinode) finishes, we get:
@@ -151,10 +153,9 @@ PUBLIC int search_file(char * path)
  *      - *ppinode: root_inode
  *      - ret val:  0 (successful)
  *
- * Currently an acceptable pathname should begin with at most one `/'
- * preceding a filename.
+ * 当前支持的路径名以“/”开头
  *
- * Filenames may contain any character except '/' and '\\0'.
+ * 文件名不能含有'/' and '\\0'.
  *
  * @param[out] filename The string for the result.
  * @param[in]  pathname The full pathname.
@@ -174,11 +175,11 @@ PUBLIC int strip_path(char * filename, const char * pathname,
 	if (*s == '/')
 		s++;
 
-	while (*s) {		/* check each character */
+	while (*s) {		/* 确认每一个字符 */
 		if (*s == '/')
 			return -1;
 		*t++ = *s++;
-		/* if filename is too long, just truncate it */
+		/* 文件名太长的话，就截断它 */
 		if (t - filename >= MAX_FILENAME_LEN)
 			break;
 	}
